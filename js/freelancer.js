1804657a153d360085fa16e1a0d6884a63e220e4
Vishal Madhvani <madhvani.vishal@gmail.com>
2016-05-05 13:28:16 -0400

    Added basic auto fading between quotes

--- js/freelancer.js
+++ js/freelancer.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
-    $('#fullpage').fullpage({responsiveHeight: 550});
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        responsiveHeight: 550,
+        afterRender: function () {
+            setInterval(function () {
+                $.fn.fullpage.moveSlideRight();
+            }, 5000);
+        },
+        onSlideLeave: function(anchorLink, index, slideIndex, direction) {
+            $.fn.fullpage.setScrollingSpeed(0);
+            $('.fp-section').find('.fp-slidesContainer').hide();
+        },
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+            $('.fp-section').find('.fp-slidesContainer').fadeIn(700);
+            $.fn.fullpage.setScrollingSpeed(700);
+        }
+    });
 });
